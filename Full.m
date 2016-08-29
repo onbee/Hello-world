335eb9c15b617867116358e7d7fc96260df69244
onbee <onbee@hotmail.com>
2016-08-29 22:17:39 +0700

    Add files via upload

---
+++ Full.m
@@ -0,0 +1,10 @@
+close all;
+clear all;
+
+% h = figure;
+% pause(0.01);
+% jf=get(h,'JavaFrame');
+% set(jf,'Maximized',1);
+
+figure(1),
+title({['fdfdfdfdf  ', num2str(52)];['nnnnnnnn',num2str(10)]});
